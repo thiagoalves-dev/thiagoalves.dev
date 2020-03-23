4c060d6a129fc4efd9dba7b33b7e341f5e8ff159
Thiago Alves <talves.ads@gmail.com>
2020-03-23 20:43:41 -0300

    menu final + background

--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -1,13 +1,9 @@
 <nav class="hidden lg:flex items-center justify-end text-lg">
     <a title="{{ $page->siteName }} Blog" href="/blog" class="ml-6 cube-palette-2 hover:cube-palette-3 {{ $page->isActive('/blog') ? 'active cube-palette-3' : '' }}">
-        Blog
+        + Posts
     </a>
 
     <a title="{{ $page->siteName }} About" href="/about" class="ml-6 cube-palette-2 hover:cube-palette-3 {{ $page->isActive('/about') ? 'active cube-palette-3' : '' }}">
-        About
-    </a>
-
-    <a title="{{ $page->siteName }} Contact" href="/contact" class="ml-6 cube-palette-2 hover:cube-palette-3 {{ $page->isActive('/contact') ? 'active cube-palette-3' : '' }}">
-        Contact
+        Sobre
     </a>
 </nav>
