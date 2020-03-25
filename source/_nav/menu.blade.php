4bcfd7413cade71ee53c97dc61f3f574babfb039
Thiago Alves <talves.ads@gmail.com>
2020-03-25 20:57:18 -0300

    estilização da página de posts

--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="hidden lg:flex items-center justify-end text-lg">
     <a title="{{ $page->siteName }} Blog" href="/blog" class="ml-6 cube-palette-2 hover:cube-palette-3 {{ $page->isActive('/blog') ? 'active cube-palette-3' : '' }}">
-        + Posts
+        Blog
     </a>
 
     <a title="{{ $page->siteName }} About" href="/about" class="ml-6 cube-palette-2 hover:cube-palette-3 {{ $page->isActive('/about') ? 'active cube-palette-3' : '' }}">
--- source/blog.blade.php
+++ source/blog.blade.php
@@ -28,27 +28,21 @@ pagination:
     @if ($pagination->pages->count() > 1)
         <nav class="flex text-base my-8">
             @if ($previous = $pagination->previous)
-                <a
-                    href="{{ $previous }}"
-                    title="Previous Page"
-                    class="bg-gray-200 hover:bg-gray-400 rounded mr-3 px-5 py-3"
-                >&LeftArrow;</a>
+                <a href="{{ $previous }}" title="Previous Page" class="bg-cube-palette-1-200 hover:bg-white cube-palette-3 rounded mr-3 px-5 py-3">
+                    &LeftArrow;
+                </a>
             @endif
 
             @foreach ($pagination->pages as $pageNumber => $path)
-                <a
-                    href="{{ $path }}"
-                    title="Go to Page {{ $pageNumber }}"
-                    class="bg-gray-200 hover:bg-gray-400 text-blue-700 rounded mr-3 px-5 py-3 {{ $pagination->currentPage == $pageNumber ? 'text-blue-600' : '' }}"
-                >{{ $pageNumber }}</a>
+                <a href="{{ $path }}" title="Go to Page {{ $pageNumber }}" class="bg-cube-palette-1-200 hover:bg-white cube-palette-3 rounded mr-3 px-5 py-3 {{ $pagination->currentPage === $pageNumber ? 'bg-white' : '' }}">
+                    {{ $pageNumber }}
+                </a>
             @endforeach
 
             @if ($next = $pagination->next)
-                <a
-                    href="{{ $next }}"
-                    title="Next Page"
-                    class="bg-gray-200 hover:bg-gray-400 rounded mr-3 px-5 py-3"
-                >&RightArrow;</a>
+                <a href="{{ $next }}" title="Next Page" class="bg-cube-palette-1-200 hover:bg-white cube-palette-3 rounded mr-3 px-5 py-3">
+                    &RightArrow;
+                </a>
             @endif
         </nav>
     @endif
