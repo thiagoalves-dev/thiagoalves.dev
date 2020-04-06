6b12b17cb89aca4860d907014f850cce6c59245e
Thiago Alves <talves.ads@gmail.com>
2020-04-06 19:24:45 -0300

    site descriptions

--- config.production.php
+++ config.production.php
@@ -3,4 +3,7 @@
 return [
     'baseUrl' => 'https://mastercode.dev',
     'production' => true,
+    'siteName' => 'Mastercode',
+    'siteDescription' => 'Blog com a finalidade de compartilhar experiências e ajudar outros desenvolvedores.',
+    'siteAuthor' => 'Mastercode Web LTDA',
 ];
