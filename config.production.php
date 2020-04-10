ceba7f59344de95cf12181c9c0560128db879381
Thiago Alves <talves.ads@gmail.com>
2020-04-10 12:53:21 -0300

    default site image to share defined

--- config.production.php
+++ config.production.php
@@ -5,7 +5,7 @@ return [
     'siteName' => 'Mastercode.dev',
     'baseUrl' => 'https://mastercode.dev',
     'siteAuthor' => 'Mastercode Web LTDA',
-    'siteImage' => 'https://mastercode.dev/assets/images/mastercode-txt.png',
+    'siteImage' => 'https://mastercode.dev/assets/images/mastercode.png',
     'siteDescription' => 'Site com a finalidade de compartilhar experiências e ajudar outros desenvolvedores.',
     'siteKeywords' => 'Full Stack, Web, Development, Desenvolvimento, Programação, Laravel, Vue.js, PHP, Vue, Sass, Front-end, Back-end, Javascript',
 ];
