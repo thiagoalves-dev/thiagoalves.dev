1df3e307b2fc93d34bf77a6a0a0d0e8bdb0b2596
Thiago Alves <talves.ads@gmail.com>
2020-05-14 18:01:03 -0300

    about page in mobile menu

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -5,5 +5,11 @@
                 Blog
             </a>
         </li>
+
+        <li class="pl-4">
+            <a title="{{ $page->siteName }} Sobre" href="/sobre" class="nav-menu__item cube-palette-2 hover:cube-palette-3 {{ $page->isActive('/sobre') ? 'active cube-palette-3' : '' }}">
+                Sobre
+            </a>
+        </li>
     </ul>
 </nav>
