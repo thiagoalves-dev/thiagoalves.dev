e913e644fbbfb1314fddbffeeafb872a0dbb7ee5
Thiago Alves <talves.ads@gmail.com>
2020-05-21 13:24:12 -0300

    Thiago's mentory page in progress

--- source/thiago/_layouts/mentory/beginner-hourly.blade.php
+++ source/thiago/_layouts/mentory/beginner-hourly.blade.php
@@ -1,17 +1,22 @@
-<div class="bg-white rounded-lg p-6 w-full md:flex shadow-lg">
-    <div class="md:text-left">
-        <h2 class="text-lg text-center text-xl mb-1">
-            <span class="w-3 h-3 inline-block bg-blue-300 align-middle rounded-full"></span>
-            Horário | Iniciante
+<div class="bg-white rounded-lg p-6 pt-2 w-full md:flex shadow-lg">
+    <div class="text-center">
+        <span class="w-3 h-3 inline-block bg-blue-300 align-middle rounded-full mx-auto"></span>
+        <h2 class="text-lg text-xl mt-0 mb-1">
+            Horário <br> Iniciante
         </h2>
         <hr class="mt-3 mb-3">
-        <div class="text-center text-gray-600">
+        <div class="text-gray-600">
             Uma hora de mentoria para quem está começando e quer tirar dúvidas.
-            <br><br>
-            Conteúdo: direcionamento, cursos, tecnologias, resolução de tarefas.
+
+            <ul class="list-none">
+                <li class="border-t">Direcionamento</li>
+                <li class="border-t">Cursos</li>
+                <li class="border-t">Tecnologias</li>
+                <li class="border-t">Resolução de tarefas</li>
+            </ul>
         </div>
         <hr class="mt-3 mb-3">
-        <div class="text-center text-gray-600">
+        <div class="text-gray-600">
             <strong>Valor: R$ 47,00</strong>
         </div>
     </div>
