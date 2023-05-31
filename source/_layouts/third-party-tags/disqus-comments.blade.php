b24e956cb679aafde99cf98a1aa0cb027c9c988a
Thiago Alves <talves.ads@gmail.com>
2023-05-31 12:41:13 -0600

    setup new disquis account

--- source/_layouts/third-party-tags/disqus-comments.blade.php
+++ source/_layouts/third-party-tags/disqus-comments.blade.php
@@ -1,17 +1,14 @@
 @if ($page->production)
-<div id="disqus_thread"></div>
-<script>
-    let disqus_config = function () {
-        this.page.url = '{{ $page->getUrl() }}';
-        this.page.identifier = '{{ $page->getDate()->format('Y-m-d H:i:s') }}';
-    };
-
-    (function() {
-        var d = document, s = d.createElement('script');
-        s.src = 'https://mastercode-dev.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    <div id="disqus_thread"></div>
+    <script>
+        (function () {
+            var d = document, s = d.createElement('script');
+            s.src = 'https://thiagoalves-dev.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
+    <noscript>
+        Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
+    </noscript>
 @endif
