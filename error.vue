f5fb57ba46ce6c105ce104a924f86610e6b3d37e
Thiago Alves <talves.ads@gmail.com>
2023-10-21 13:31:39 -0600

    404 page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+    <NuxtLayout>
+        <div class="content-container text-center">
+            <h1>Página não encontrada!</h1>
+
+            <NuxtLink to="/" class="back">
+                Voltar para a página inicial!
+            </NuxtLink>
+        </div>
+    </NuxtLayout>
+</template>
+
+<style lang="scss" scoped>
+a.back {
+    @apply bg-transparent text-gray-600 font-semibold py-2 px-4 border rounded hover:border-gray-600;
+}
+</style>
