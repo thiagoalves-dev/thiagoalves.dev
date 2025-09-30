2014aed086a52a0e48cfd480b27fe90a33d4368f
Thiago Alves <talves.ads@gmail.com>
2025-09-29 19:03:54 -0600

    Set tailwind up

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-        <NuxtRouteAnnouncer/>
-        <NuxtWelcome/>
+    <div style="height: 2000px;" class="bg-amber-300">
+        Thiago Alves
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
-    devtools: { enabled: true }
+    css: ['~/assets/css/main.css'],
+    devtools: { enabled: true },
+    vite: {
+        plugins: [
+            tailwindcss()
+        ]
+    }
 })
